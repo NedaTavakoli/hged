0fc2adff86f8dbd5a30f6faf03ea99e98dd40097
NedaTavakoli <tavakoli.neda@gmail.com>
2022-10-04 13:04:31 -0700

    fixed bug of length alpha

--- get_POS_substring.sh
+++ get_POS_substring.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
 
-# Change this line according to your projcet directory
-cd /storage/coda1/p-saluru8/0/ntavakoli6/hged
-id=22
-alpha=3
-project_dir=$(pwd)  #project top-level directory
-cd ${project_dir}/data
-DATA=$(pwd)
-cd ${project_dir}/software
-software_dir=$(pwd)
-cd ${project_dir}
-bcftools=${software_dir}/bcftools-1.9/bcftools
-vcftools=${software_dir}/vcftools-0.1.16/bin/vcftools
-tabix=${software_dir}/htslib-1.12/tabix
-bgzip=${software_dir}/htslib-1.12/bgzip
-samtools=${software_dir}/samtools-1.12/samtools
-cd ${DATA}
 # ***************************************************************************************
-# v=51214426
-# id=22
+## TODO: CHANGE THIS AS ARGUMENT
 id=22
-alpha=100
+alpha=10
 # ***************************************************************************************
 
 ref=hs37d5
@@ -42,22 +25,19 @@ do
         sample=${samples[sample_index]}
         if [ ${h1} -ne 0 ]; then
             substring1=($($samtools faidx ${ref}.fa ${id}:${v}-$((${v} + ${alpha})) | $bcftools consensus -s ${sample} -H 1  chr${id}.vcf.gz | tail -1))
+            t=$((${alpha}))
+            len_s1=${#substring1}
+            min_v1=$((t<len_s1? t : len_s1))
+            a+=(${substring1:0:min_v})
         fi
-        t=$((${alpha}))
-        len_s1=${#substring1}
-        min_v1=$((t<len_s1? t : len_s1))
-        a+=(${substring1:0:min_v})
-        # min of ${alpha}-1  and len(substring1)
         if [ ${h2} -ne 0 ]; then
             substring2=($($samtools faidx ${ref}.fa ${id}:${v}-$((${v} + ${alpha})) | $bcftools consensus -s ${sample} -H 2  chr${id}.vcf.gz | tail -1))
+            len_s2=${#substring2}
+            min_v2=$((t<len_s2? t : len_s2))
+            a+=(${substring2:0:min_v2})
         fi
-        len_s2=${#substring2}
-        min_v2=$((t<len_s2? t : len_s2))
-        a+=(${substring2:0:min_v2})
     done
-    #echo ${a[@]}
     uniq_s=$(printf "%s\n" "${a[@]}" | sort -u)
-    #echo $uniq_s >> chr${id}_pos_substrings.txt
     echo $uniq_s
 done >> chr${id}_pos_substrings_len_${alpha}.txt
 
