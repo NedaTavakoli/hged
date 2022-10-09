666de599a8a3cb43dfb4ba25e03b344741457c43
NedaTavakoli <tavakoli.neda@gmail.com>
2022-10-08 18:45:35 -0700

    upate c++ for bcftools

--- src/ilp_snp_indels.cpp
+++ src/ilp_snp_indels.cpp
@@ -12,8 +12,10 @@
 #include <unordered_map>
 #include "ext/prettyprint.hpp"
 #include "common.hpp"
+#include "boost/process.hpp"
 #include "gurobi_c++.h"
 
+namespace bp = boost::process;
 /********* Helper functions ******/
 
 /**
@@ -63,8 +65,11 @@ void extract_pos_substring (const std::string &vcf_file, const std::string &fast
       //  arr=($($bcftools view -H -r 22:${v} chr${id}.vcf.gz| awk -F"\t" '{split($0, header, "\t");} \
         {for (i=10; i<=NF; i++) {if (gsub(/0\|1|1\|0|0\/1|1\/0/, "", $(i))==1) {printf header[i]",";printf i-10"\t"} if (i==NF) {printf "\n"}}}'))  
       srand(time(0)); int random3 = rand() % 100000;  
-      std::string tmp_file3 = ".VF." + std::to_string(random3) + ".txt";
-      std::string cmd3 = std::string(TOSTRING(BCFTOOLSPATH)) + " view  -H -r " + std::string(TOSTRING(chr)) + ":" + std::string(TOSTRING(variant_pos[i])) + " " + vcf_file + " | awk -F" + "\t" + "{split($0, header," + "\t"+ ");{for (i=10; i<=NF; i++) {if (gsub(/0" + "\" + "|" + "1|1" + "\" + "|0|0" + "\/" + "1|1" + "\/0/" + ", "+ ";"+ $(i))==1) {printf header[i]+ "," + ;printf i-10" "} if (i==NF) {printf " "}}}'))}" + " >  " + tmp_file3;
+      std::string tmp_file3 = ".hged." + std::to_string(random3) + ".txt";
+      std::string cmd3 = std::string(TOSTRING(BCFTOOLSPATH)) + " view  -H -r " + std::string(TOSTRING(chr)) + ":" + std::string(TOSTRING(variant_pos[i])) + " " + vcf_file  + " >  " + tmp_file3; 
+      // std::string cmd4 = " awk -F\"t\" ;
+      
+     
       std::cout << "INFO, hged::main, extracting pos from variant position file using command: " << cmd << std::endl;
       std::system(cmd3.c_str());
 
