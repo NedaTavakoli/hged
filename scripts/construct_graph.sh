99af65961092055cdd8cd495ec73d4e032475d63
NedaTavakoli <tavakoli.neda@gmail.com>
2022-10-11 08:12:09 -0700

    update graph and substrings

--- scripts/construct_graph.sh
+++ scripts/construct_graph.sh
@@ -24,7 +24,8 @@ alpha=$2
 project_dir=$(pwd)
 cd data
 DATA=$(pwd)
-cd ../build
+# cd ../build
+cd ../software
 software_dir=$(pwd)
 bcftools=${software_dir}/bcftools-1.9/bcftools
 samtools=${software_dir}/samtools-1.12/samtools
@@ -43,13 +44,9 @@ t=$((${end}))
 min_last=$((len_REF_end<t? len_REF_end: t))
 end=$(($end+$min_last-1))
 
-$samtools faidx hs37d5.fa ${id}:${start}-${end} >  ${graph}/linear_bc_chr${id}_alpha_${alpha}.fa
-sed '1d'  ${graph}/linear_bc_chr${id}_alpha_${alpha}.fa >  ${graph}/linear_bc_chr${id}_alpha_${alpha}_u.fa # remove the first line
-tr -d "[:space:]" <  ${graph}/linear_bc_chr${id}_alpha_${alpha}_u.fa > ${graph}/linear_bc_chr${id}_alpha_${alpha}_updated.fa  # remove all the white spaces
-REF_l=($(cat ${graph}/linear_bc_chr${id}_alpha_${alpha}_updated.fa))
+REF_l=$($samtools faidx hs37d5.fa ${id}:${start}-${end} |  sed '1d' | tr -d "[:space:]") # remove the first linea and remove whitespaces
 leng=$(echo ${REF_l[@]} | wc -c)
-# echo ${REF_l:0:20})}
-
+# echo ${REF_l:0:20}
 num_vertice_linear_bc=$((${leng}+1))
 num_alt_vertices=$(cat chr${id}_snps_indel_POS_REF_ALT.txt | awk ' length($3)>1'| awk '{sum = 0; t = split($3, arr, ","); for(i = 1; i <= t; i++) sum+=(length(arr[i])-1);print sum}' | awk '{ sum_t += $1 } END { print sum_t }')
 Total_vertices=$(($num_vertice_linear_bc + $num_alt_vertices))
--- scripts/get_POS_substrings.sh
+++ scripts/get_POS_substrings.sh
@@ -23,8 +23,7 @@ alpha=$2
 project_dir=$(pwd)
 cd data
 DATA=$(pwd)
-# cd ../build
-cd ../software 
+cd ../build
 software_dir=$(pwd)
 bcftools=${software_dir}/bcftools-1.9/bcftools
 samtools=${software_dir}/samtools-1.12/samtools
@@ -41,24 +40,21 @@ samples=($($bcftools query -l chr${id}.vcf.gz)) # array of samples, index from 0
 
 # For each variant position 
 for v in "${variant_positions[@]}"
-# for v in "${variant_positions:0:10}" # specify a range of variant position just for testing
+#for v in "${variant_positions:0:10}" # specify a range of variant position just for testing
 do
     a=($v)
     arr=($($bcftools view -H -r 22:${v} chr${id}_snps_indels.vcf.gz |  awk -F"\t" '{split($0, header, "\t");} \
-        {for (i=10; i<=NF; i++) {if ((gsub(/0\|0|0\/0|/, "", $(i)) !=1))  {printf header[i]",";printf i-10"\t"} if (i==NF) {printf "\n"}}}'))
+        {for (i=10; i<=NF; i++) {if ((gsub(/0\|0|0\/0|/, "", $(i)) !=1))  {printf header[i]",";printf i-10"\t"} if (i==NF) {printf "\n"}}}'))   
 
     for gt in "${arr[@]}"
     do
-        h1=${gt:0:1}
+        h1=${gt:0:1} 
         h2=${gt:2:2}  # it has comma at the end 
         h2=($(echo $h2| sed 's/\(.*\),/\1 /'))  # remove the comma at the end
         sample_index=${gt:4:5}
         sample=${samples[sample_index]}
         if [ ${h1} -ne 0 ]; then
-            $samtools faidx ${ref}.fa ${id}:${v}-$((${v} + ${alpha}-1)) | $bcftools consensus -s ${sample} -H 1 ${MyVariants} > tmp_${v}_alpha_${alpha}_sample_${sample}_h1.txt
-            sed '1d' tmp_${v}_alpha_${alpha}_sample_${sample}_h1.txt > tmp_${v}_alpha_${alpha}_sample_${sample}_h1_u.txt # remove the first line
-            tr -d "[:space:]" < tmp_${v}_alpha_${alpha}_sample_${sample}_h1_u.txt > tmp_${v}_alpha_${alpha}_sample_${sample}_h1_updated.txt  # remove all the white spaces
-            s1=($(cat tmp_${v}_alpha_${alpha}_sample_${sample}_h1_updated.txt))
+            s1=$($samtools faidx ${ref}.fa ${id}:${v}-$((${v} + ${alpha}-1)) | $bcftools consensus -s ${sample} -H 1 ${MyVariants} |  sed '1d' | tr -d "[:space:]")
             len_s1=$(echo ${s1[@]} | wc -c)
             t=$((${alpha}))
             min_v1=$((t<len_s1? t : len_s1))
@@ -66,15 +62,11 @@ do
             rm -f tmp_*
         fi
         if [ ${h2} -ne 0 ]; then
-            $samtools faidx ${ref}.fa ${id}:${v}-$((${v} + ${alpha}-1)) | $bcftools consensus -s ${sample} -H 2 ${MyVariants} > tmp_${v}_alpha_${alpha}_sample_${sample}_h2.txt
-            sed '1d' tmp_${v}_alpha_${alpha}_sample_${sample}_h2.txt > tmp_${v}_alpha_${alpha}_sample_${sample}_h2_u.txt # remove the first line
-            tr -d "[:space:]" < tmp_${v}_alpha_${alpha}_sample_${sample}_h2_u.txt > tmp_${v}_alpha_${alpha}_sample_${sample}_h2_updated.txt  # remove all the white spaces
-            s2=($(cat tmp_${v}_alpha_${alpha}_sample_${sample}_h2_updated.txt))
+            s2=$($samtools faidx ${ref}.fa ${id}:${v}-$((${v} + ${alpha}-1)) | $bcftools consensus -s ${sample} -H 2 ${MyVariants} |  sed '1d' | tr -d "[:space:]")
             len_s2=$(echo ${s2[@]} | wc -c)
             t=$((${alpha}))
             min_v2=$((t<len_s2? t : len_s2))
             a+=(${s2:0:min_v2})
-            rm -f tmp_*
         fi
     done
     # echo ${a[@]} 
